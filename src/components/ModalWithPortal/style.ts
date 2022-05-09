c33023724edb537ab93bc8fb6cec6bd0bdbff61d
Parksangwoo <psu8430@gmail.com>
2022-05-10 01:25:48 +0900

    Feat: Portal을 활용한 Modal 생성

---
+++ src/components/ModalWithPortal/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const PortalWrapper = styled.div`
+  position: relative;
+  display: fixed;
+  justify-content: center;
+`;
+
+export const ModalButton = styled.button`
+  margin: auto;
+  background: lightgray;
+  padding: 10px 15px;
+  border: 0;
+  border-radius: 50px;
+  box-shadow: 0 0 10px 0 #00000030;
+  font-size: 18px;
+`;
